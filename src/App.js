8a6a70cce5ee5cea67bb42a874404794eb335779
Ajay Prakash <21376002@pondiuni.ac.in>
2026-02-16 15:17:15 +0530

    exploring GET,POST using app.use and app.get and app.post

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ const express = require("express");
 const app = express();
 
 // Request Handler
-app.use("/world", (req, res) => {
-  res.send("Namaste from Dashboard");
+// app.use("/world", (req, res) => {
+//   res.send("Namaste from Dashboard");
+// });
+// app.use("/hello", (req, res) => {
+//   res.send("Hello hello hello");
+// });
+// app.use("/test", (req, res) => {
+//   res.send("Hello World from server");
+// });
+
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Ajay", lastName: "Prakash" });
 });
-app.use("/hello", (req, res) => {
-  res.send("Hello hello hello");
+
+app.post("/user", (req, res) => {
+  // Saving  Data to the Database
+  res.send("Data successfully saved to the database");
 });
+
+// This will match all the HTTP methods API Calls to /test
 app.use("/test", (req, res) => {
-  res.send("Hello World from server");
+  res.send("Namaste Ajay");
 });
 
 const port = 3000;
